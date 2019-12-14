54c3587f390efc59d1f771eea7179ba4f9cc418a
Ken Diehl <diehlkj.99@gmail.com>
2019-12-13 21:58:07 -0500

    api call, page routing, card mapping done/functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/characters">Characters</Link>
+      </nav>
+
+      <Route exact path="/" component={WelcomePage}/>
+
+      <Route exact path="/characters" 
+          render={(routeProps) => {
+            return <CharacterList {...routeProps} />
+          }
+        }
+      />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [ramapi, setRamapi] = useState([]);
+  const [apiInfo, setApiInfo] = useState();
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setRamapi(response.data.results); // Array of objects
+        setApiInfo(response.data.info); // Object: -count -pages -next (Gives url for next page... could just take pages as a var and for loop that number of times incrementing the ?page=X query )
+        console.log(response); 
+      })
+      .catch(error => console.log(`Uh oh... ${error}`));
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {ramapi.map(character => {
+        return <CharacterCard name={character.name} status={character.status} key={character.id}/> //Will add gender species status and image later
+      })}
+      
     </section>
   );
 }
